e1df21c1f3b2b686999878a477c557025028fb06
hojas <chenyuanxiang.x@gmail.com>
2026-06-12 00:06:11 +0800

    添加默认数据

--- src/pages/DemoPage.vue
+++ src/pages/DemoPage.vue
@@ -5,7 +5,7 @@ import RichTextEditor from '../editor/RichTextEditor.vue'
 import EdmContentViewer from '../viewer/EdmContentViewer.vue'
 
 /** 编辑器内容（HTML） */
-const content = ref('<p>欢迎编辑 EDM 内容。</p>')
+const content = ref('<p>欢迎编辑 EDM 内容。</p><edm-image class="ql-edm-image ql-edm-loaded" data-edm-id="1781193873184-3ufx39xl" data-edm-type="image" data-attachment-id="1781193873184" data-file-name="a.png" title="a.png" data-mime-type="image/png" data-file-size="252"><img data-src="https://c8mqsr1j97cqhcch.public.blob.vercel-storage.com/1781193873184-3ufx39xl" alt="a.png" data-edm-id="1781193873184-3ufx39xl" data-edm-type="image" data-attachment-id="1781193873184" data-file-name="a.png" title="a.png" data-mime-type="image/png" data-file-size="252" src="https://c8mqsr1j97cqhcch.public.blob.vercel-storage.com/1781193873184-3ufx39xl" style="width: 213px; height: 258px; max-width: none;"></edm-image><p><br></p>')
 /** 编辑器中已使用的 EDM 附件列表 */
 const attachments = ref<EdmAttachment[]>([])
 
